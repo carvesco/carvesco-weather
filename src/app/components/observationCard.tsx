3280661a6c64d024a01d5ab5a2e400e2cbd7b5fc
carvesco <carvesco@gmail.com>
2024-11-13 12:42:17 -0500

    added aditional observations cards

--- src/app/components/observationCard.tsx
+++ src/app/components/observationCard.tsx
@@ -1,9 +1,58 @@
-const ObservationCard = ({ data }: any) => {
+interface ObservationCardProps {
+  title?: string;
+  value?: number;
+  icon?: React.ReactNode;
+  units?: string;
+}
+
+export const ObservationCard = ({
+  title,
+  icon,
+  value,
+  units,
+}: ObservationCardProps) => {
   return (
-    <div className="rounded-xl bg-second-foreground text-second-background p-5">
-      <h2 className="text-xl">Title</h2>
+    <div className="flex flex-row rounded-xl items-center gap-2 bg-gradient-to-t from-background to-second-background text-second-foreground p-5">
+      {icon}
+      <h2 className="text-xl  ">{title}:</h2>
+      <h2 className="text-xl text-foreground font-semibold">{value}</h2>
+      <h2 className="text-xl text-foreground font-semibold">{units}</h2>
     </div>
   );
 };
 
-export default ObservationCard;
+interface ObservationCardMultiDataProps {
+  title?: string;
+  value?: Record<string, any>;
+  icon?: React.ReactNode;
+  units?: Record<string, any>;
+}
+
+export const ObservationCardMultiData = ({
+  title,
+  icon,
+  value,
+  units,
+}: ObservationCardMultiDataProps) => {
+  return (
+    <div className="flex flex-row rounded-xl items-center justify-center gap-2 bg-gradient-to-t from-background to-second-background text-second-foreground p-5">
+      {icon}
+      {title && <h2 className="text-xl  ">{title}:</h2>}
+      <div className="flex flex-col items-end">
+        {Object.entries(value || {}).map(([key, val]) => (
+          <div key={key} className="flex flex-row">
+            <h2 className="text-xl">{key}:</h2>
+            <h2 className="text-xl text-foreground font-semibold">{val}</h2>
+          </div>
+        ))}
+      </div>
+      <div className="flex flex-col">
+        {Object.entries(units || {}).map(([key, val]) => (
+          <div key={key} className="flex flex-row">
+            <h2 className="text-xl text-foreground font-semibold">{val}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
--- src/app/weather/[location]/page.tsx
+++ src/app/weather/[location]/page.tsx
@@ -1,11 +1,38 @@
 "use client";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
-import ObservationCard from "../../components/observationCard";
+import {
+  ObservationCard,
+  ObservationCardMultiData,
+} from "../../components/observationCard";
 import Overview from "../../components/overview";
+import { PiGaugeFill } from "react-icons/pi";
+import { WiHumidity } from "react-icons/wi";
+import { MdVisibility } from "react-icons/md";
+import { FaCloud } from "react-icons/fa";
+import { FaWind } from "react-icons/fa";
+import { TbSunset2 } from "react-icons/tb";
 const Page = () => {
   const params = useParams<{ location: string }>();
-  const [weatherData, setWeatherData] = useState(null);
+  interface WeatherData {
+    clouds: { all: number };
+    main: {
+      pressure: number;
+      humidity: number;
+    };
+    visibility: number;
+    wind: {
+      speed: number;
+      deg: number;
+    };
+    sys: {
+      sunrise: number;
+      sunset: number;
+    };
+    timezone: number;
+  }
+
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchWeatherData = async () => {
@@ -17,6 +44,15 @@ const Page = () => {
           throw new Error("Network response was not ok");
         }
         const data = await response.json();
+        // Convert sunrise and sunset timestamps to local time
+        const sunrise = new Date((data.sys.sunrise + data.timezone) * 1000)
+          .toUTCString()
+          .slice(-12, -7);
+        const sunset = new Date((data.sys.sunset + data.timezone) * 1000)
+          .toUTCString()
+          .slice(-12, -7);
+        data.sys.sunrise = sunrise;
+        data.sys.sunset = sunset;
         console.log(data);
         setWeatherData(data);
       } catch (error) {
@@ -37,10 +73,64 @@ const Page = () => {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-background">
+      <button className="rounded-md bg-red-400 p-4 font-semibold text-second-background mt-10 ml-10">
+        <a href="/">Return</a>
+      </button>
+      <div className="flex flex-col items-center min-h-screen bg-background">
         <Overview data={weatherData} />
-        <div className="grid grid-cols-4 items-center w-3/4 gap-4">
-          <ObservationCard />
+        <h1 className="text-3xl mt-4 font-bold">Weather Observations</h1>
+        <div className="grid grid-cols-4 items-center w-3/4 gap-4 mt-6">
+          {weatherData?.main?.pressure && (
+            <ObservationCard
+              title={"Pressure"}
+              value={weatherData?.main?.pressure}
+              units={"hPa"}
+              icon={<PiGaugeFill style={{ height: 50, width: 50 }} />}
+            />
+          )}
+          {weatherData?.main?.humidity && (
+            <ObservationCard
+              title={"Humidty"}
+              value={weatherData?.main?.humidity}
+              units={"%"}
+              icon={<WiHumidity style={{ height: 50, width: 50 }} />}
+            />
+          )}
+          {weatherData?.visibility && (
+            <ObservationCard
+              title={"Visibility"}
+              value={weatherData?.visibility}
+              units={"m"}
+              icon={<MdVisibility style={{ height: 50, width: 50 }} />}
+            />
+          )}
+          {weatherData?.clouds?.all && (
+            <ObservationCard
+              title={"Cloudiness"}
+              value={weatherData?.clouds?.all}
+              units={"%"}
+              icon={<FaCloud style={{ height: 50, width: 50 }} />}
+            />
+          )}
+        </div>
+        <div className="grid grid-cols-2 items-center w-2/4 gap-4 mt-6">
+          {weatherData?.wind && (
+            <ObservationCardMultiData
+              title={"Wind"}
+              value={weatherData?.wind}
+              units={{ speed: "m/s", deg: "°" }}
+              icon={<FaWind style={{ height: 50, width: 50 }} />}
+            />
+          )}
+          {weatherData && (
+            <ObservationCardMultiData
+              value={{
+                sunrise: weatherData?.sys.sunrise,
+                sunset: weatherData?.sys.sunset,
+              }}
+              icon={<TbSunset2 style={{ height: 50, width: 50 }} />}
+            />
+          )}
         </div>
       </div>
     </>
