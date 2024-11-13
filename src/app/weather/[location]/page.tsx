e8360fe5869bdf75b3445047a04bc0edcbec2562
carvesco <carvesco@gmail.com>
2024-11-13 15:30:15 -0500

    added city not found

--- src/app/weather/[location]/page.tsx
+++ src/app/weather/[location]/page.tsx
@@ -34,6 +34,7 @@ const Page = () => {
 
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(0);
   useEffect(() => {
     const fetchWeatherData = async () => {
       try {
@@ -41,6 +42,10 @@ const Page = () => {
           `http://api.openweathermap.org/data/2.5/weather?q=${params.location}&APPID=${process.env.NEXT_PUBLIC_WEATHER_API_KEY}&units=metric`
         );
         if (!response.ok) {
+          if (response.status === 404) {
+            setError(response.status);
+            throw new Error("City not found");
+          }
           throw new Error("Network response was not ok");
         }
         const data = await response.json();
@@ -72,66 +77,80 @@ const Page = () => {
 
   return (
     <>
-      <button className="rounded-md bg-red-400 p-4 font-semibold text-second-background mt-10 ml-10">
-        <a href="/">Return</a>
-      </button>
-      <div className="flex flex-col items-center min-h-screen bg-background">
-        <Overview data={weatherData} />
-        <h1 className="text-3xl mt-4 font-bold">Weather Observations</h1>
-        <div className="grid grid-cols-4 items-center w-3/4 gap-4 mt-6">
-          {weatherData?.main?.pressure && (
-            <ObservationCard
-              title={"Pressure"}
-              value={weatherData?.main?.pressure}
-              units={"hPa"}
-              icon={<PiGaugeFill style={{ height: 50, width: 50 }} />}
-            />
-          )}
-          {weatherData?.main?.humidity && (
-            <ObservationCard
-              title={"Humidty"}
-              value={weatherData?.main?.humidity}
-              units={"%"}
-              icon={<WiHumidity style={{ height: 50, width: 50 }} />}
-            />
-          )}
-          {weatherData?.visibility && (
-            <ObservationCard
-              title={"Visibility"}
-              value={weatherData?.visibility}
-              units={"m"}
-              icon={<MdVisibility style={{ height: 50, width: 50 }} />}
-            />
-          )}
-          {weatherData?.clouds?.all && (
-            <ObservationCard
-              title={"Cloudiness"}
-              value={weatherData?.clouds?.all}
-              units={"%"}
-              icon={<FaCloud style={{ height: 50, width: 50 }} />}
-            />
-          )}
+      {weatherData && (
+        <>
+          <button className="rounded-md bg-red-400 p-4 font-semibold text-second-background mt-10 ml-10">
+            <a href="/">Return</a>
+          </button>
+          <div className="flex flex-col items-center min-h-screen bg-background">
+            <Overview data={weatherData} />
+            <h1 className="text-3xl mt-4 font-bold">Weather Observations</h1>
+            <div className="grid grid-cols-4 items-center w-3/4 gap-4 mt-6">
+              {weatherData?.main?.pressure && (
+                <ObservationCard
+                  title={"Pressure"}
+                  value={weatherData?.main?.pressure}
+                  units={"hPa"}
+                  icon={<PiGaugeFill style={{ height: 50, width: 50 }} />}
+                />
+              )}
+              {weatherData?.main?.humidity && (
+                <ObservationCard
+                  title={"Humidty"}
+                  value={weatherData?.main?.humidity}
+                  units={"%"}
+                  icon={<WiHumidity style={{ height: 50, width: 50 }} />}
+                />
+              )}
+              {weatherData?.visibility && (
+                <ObservationCard
+                  title={"Visibility"}
+                  value={weatherData?.visibility}
+                  units={"m"}
+                  icon={<MdVisibility style={{ height: 50, width: 50 }} />}
+                />
+              )}
+              {weatherData?.clouds?.all && (
+                <ObservationCard
+                  title={"Cloudiness"}
+                  value={weatherData?.clouds?.all}
+                  units={"%"}
+                  icon={<FaCloud style={{ height: 50, width: 50 }} />}
+                />
+              )}
+            </div>
+            <div className="grid grid-cols-2 items-center w-2/4 gap-4 mt-6">
+              {weatherData?.wind && (
+                <ObservationCardMultiData
+                  title={"Wind"}
+                  value={weatherData?.wind}
+                  units={{ speed: "m/s", deg: "°" }}
+                  icon={<FaWind style={{ height: 50, width: 50 }} />}
+                />
+              )}
+              {weatherData && (
+                <ObservationCardMultiData
+                  value={{
+                    sunrise: weatherData?.sys.sunrise,
+                    sunset: weatherData?.sys.sunset,
+                  }}
+                  icon={<TbSunset2 style={{ height: 50, width: 50 }} />}
+                />
+              )}
+            </div>
+          </div>
+        </>
+      )}
+      {error === 404 && (
+        <div className="flex flex-col items-center min-h-screen bg-background mt-52">
+          <h1 className="text-3xl mt-4 font-bold text-red-500">
+            City not found. Please try again.
+          </h1>
+          <button className="rounded-md bg-red-400 p-4 font-semibold text-second-background mt-10">
+            <a href="/">Return</a>
+          </button>
         </div>
-        <div className="grid grid-cols-2 items-center w-2/4 gap-4 mt-6">
-          {weatherData?.wind && (
-            <ObservationCardMultiData
-              title={"Wind"}
-              value={weatherData?.wind}
-              units={{ speed: "m/s", deg: "°" }}
-              icon={<FaWind style={{ height: 50, width: 50 }} />}
-            />
-          )}
-          {weatherData && (
-            <ObservationCardMultiData
-              value={{
-                sunrise: weatherData?.sys.sunrise,
-                sunset: weatherData?.sys.sunset,
-              }}
-              icon={<TbSunset2 style={{ height: 50, width: 50 }} />}
-            />
-          )}
-        </div>
-      </div>
+      )}
     </>
   );
 };
